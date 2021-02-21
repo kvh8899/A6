85b46ef82d3375fe8bb449932e8059b65f2d2f9d
Haley Hammock <hhammock@ucsd.edu>
2021-02-20 18:35:48 -0800

    fix json file

--- routes/addRecords.js
+++ routes/addRecords.js
@@ -1,13 +1,16 @@
-var data = require("../records.json");
+var records = require("../records.json");
 
 exports.addData = function(request, response) {    
 	// Your code goes here
 	var recordData = {
 		"date": request.query.date,
 		"title": request.query.title,
-		"subject": request.query.subject
+		"subject": request.query.subject,
+		"time": request.query.time,
+		"scale": request.query.scale
 	};
-	data.records.push(recordData);
-	console.log(data);
-	response.render("record", data);
+	records.records.push(recordData);
+	console.log(records);
+	console.log(records["data"]);
+	response.render("record", records);
 }
